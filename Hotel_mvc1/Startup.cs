e06c4711385c8fd130282c46133dc486a3ddff0b
FilipTisljar <tisljar.filip@gmail.com>
2017-10-20 17:32:00 +0200

    Add project files.

---
+++ Hotel_mvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotel_mvc1.Startup))]
+namespace Hotel_mvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
